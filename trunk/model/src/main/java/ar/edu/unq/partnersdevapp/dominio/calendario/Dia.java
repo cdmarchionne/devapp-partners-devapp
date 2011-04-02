9634501c90df93a310d47cc7fe9162c01885a74e
palazzo.leonardo@gmail.com <palazzo.leonardo@gmail.com@0efc6b8a-07c5-4736-7702-24312db4aa60>
2011-04-02 04:43:44 +0000

    Se trabajo sobre el paquete de calendario.

--- trunk/model/src/main/java/ar/edu/unq/partnersdevapp/dominio/calendario/Dia.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.partnersdevapp.dominio.calendario;
-
-/**
- * TODO: description
- */
-public enum Dia {
-    cal, col
-
-}
---
+++ trunk/model/src/test/java/ar/edu/unq/partnersdevapp/ListaUtilTest.java
@@ -0,0 +1,43 @@
+package ar.edu.unq.partnersdevapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+import ar.edu.unq.partnersdevapp.dominio.utils.ListaUtils;
+
+/**
+ * TODO: description
+ */
+public class ListaUtilTest extends TestCase {
+    List<Integer> intlist = new ArrayList<Integer>();
+
+    @Override
+    protected void setUp() throws Exception {
+        intlist.add(1);
+        intlist.add(2);
+        intlist.add(3);
+        intlist.add(4);
+        intlist.add(5);
+    }
+
+    public void testSacarMayoresDe() {
+        List<Integer> deberiaSer = new ArrayList<Integer>();
+        deberiaSer.add(1);
+        deberiaSer.add(2);
+
+        ListaUtils.sacarMayoresDe(intlist, 2);
+        assertTrue(intlist.equals(deberiaSer));
+    }
+
+    public void testSacarMenoresDe() {
+        List<Integer> deberiaSer = new ArrayList<Integer>();
+        deberiaSer.add(2);
+        deberiaSer.add(3);
+        deberiaSer.add(4);
+        deberiaSer.add(5);
+
+        ListaUtils.sacarMenoresDe(intlist, 2);
+        assertTrue(intlist.equals(deberiaSer));
+    }
+}
