2d3837d305ad6f914314f7d4edc7da0b7945e0e4
cdmarchionne@gmail.com <cdmarchionne@gmail.com@0efc6b8a-07c5-4736-7702-24312db4aa60>
2011-04-11 23:08:22 +0000

    Plan de Carrera

---
+++ trunk/model/src/main/java/ar/edu/unq/partnersdevapp/dominio/carrera/Categoria.java
@@ -0,0 +1,74 @@
+package ar.edu.unq.partnersdevapp.dominio.carrera;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO: description
+ */
+public class Categoria {
+
+    private static List<String> listaCategoria = new ArrayList<String>();
+
+    public Categoria() {
+        super();
+    }
+
+    public static String nombreCategoria(final Integer indice) {
+        return listaCategoria.get(indice);
+    }
+
+    public static Integer indiceCategoria(final String nombreCategoria) {
+        return listaCategoria.indexOf(nombreCategoria);
+    }
+
+    public static void addCategoria(final String nombre) {
+        addCategoria(listaCategoria.size(), nombre);
+    }
+
+    public static void addCategoria(final Integer posicion, final String nombre) {
+        if (!listaCategoria.contains(nombre) && 0 <= posicion && posicion <= listaCategoria.size()) {
+            listaCategoria.add(posicion, nombre);
+        }
+    }
+
+    public static boolean removeCategoria(final String nombre) {
+        return listaCategoria.remove(nombre);
+    }
+
+    public static void renameCategoria(final String nombreViejo, final String nombreNuevo) {
+        listaCategoria.set(listaCategoria.indexOf(nombreViejo), nombreNuevo);
+    }
+
+    public static List<String> getCategorias() {
+        return listaCategoria;
+    }
+
+    public static Integer bajarCategoria(final Integer valor) {
+        if (0 < valor && valor - 1 < listaCategoria.size())
+            return valor - 1;
+        return valor;
+    }
+
+    public static Integer subirCategoria(final Integer valor) {
+        if (0 <= valor && valor + 1 < listaCategoria.size())
+            return valor + 1;
+        return valor;
+
+    }
+
+    @SuppressWarnings("null")
+    public static void main(final String[] args) {
+        Categoria.addCategoria("Basico");
+        Categoria.addCategoria("Medio");
+        Categoria.addCategoria("Experto");
+
+        for (String nombreCategoria : Categoria.getCategorias()) {
+            System.out.println(nombreCategoria);
+        }
+
+        Integer experiencia = 0;
+        System.out.println(experiencia + " " + Categoria.nombreCategoria(experiencia));
+
+    }
+}
--- trunk/model/src/main/java/ar/edu/unq/partnersdevapp/dominio/carrera/Tecnologia.java
+++ trunk/model/src/main/java/ar/edu/unq/partnersdevapp/dominio/carrera/Tecnologia.java
@@ -1,12 +1,36 @@
 package ar.edu.unq.partnersdevapp.dominio.carrera;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * TODO: description
  */
 public class Tecnologia {
-    String nombre;
 
-    public Tecnologia(final String nombre) {
-        this.nombre = nombre;
+    private static Set<String> tecnologia = new HashSet<String>();
+
+    public static boolean addTecnologia(final String nombre) {
+        return tecnologia.add(nombre);
+    }
+
+    public static boolean removeTecnologia(final String nombre) {
+        return tecnologia.remove(nombre);
+    }
+
+    public static boolean esTecnologia(final String nombre) {
+        return tecnologia.contains(nombre);
+    }
+
+    public static String getTecnologias() {
+        return tecnologia.toString();
+    }
+
+    public static void main(final String[] args) {
+        Tecnologia.addTecnologia("JAVA");
+        Tecnologia.addTecnologia("Poo");
+
+        System.out.println(Tecnologia.getTecnologias());
+
     }
 }
