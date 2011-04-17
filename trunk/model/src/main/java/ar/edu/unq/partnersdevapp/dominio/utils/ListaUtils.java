67a6140f6c4f021d229c374c5b2f3a06243c8645
cdmarchionne@gmail.com <cdmarchionne@gmail.com@0efc6b8a-07c5-4736-7702-24312db4aa60>
2011-04-17 16:00:37 +0000

    Se arreglo algunos errores de sintaxis

--- trunk/model/src/main/java/ar/edu/unq/partnersdevapp/dominio/utils/ListaUtils.java
+++ trunk/model/src/main/java/ar/edu/unq/partnersdevapp/dominio/utils/ListaUtils.java
@@ -13,18 +13,18 @@ public class ListaUtils<T> {
         throw new UnsupportedOperationException();
     }
 
-    static public List<Integer> sacarMayoresDe(final List<Integer> list, final int valor) {
+    public static List<Integer> sacarMayoresDe(final List<Integer> list, final int valor) {
         list.retainAll(ListaUtils.sacarMayoresDeAux(list, valor));
         return list;
     }
 
-    static public List<Integer> sacarMenoresDe(final List<Integer> list, final int valor) {
+    public static List<Integer> sacarMenoresDe(final List<Integer> list, final int valor) {
         list.removeAll(ListaUtils.sacarMayoresDeAux(list, valor - 1));
         return list;
     }
 
     /** Devuelve la lista sin los números maroyes al parametro */
-    static private List<Integer> sacarMayoresDeAux(final List<Integer> list, final int valor) {
+    private static List<Integer> sacarMayoresDeAux(final List<Integer> list, final int valor) {
         List<Integer> intlistRes = new ArrayList<Integer>();
         for (Integer integer : list) {
             if (integer <= valor) {
@@ -34,7 +34,7 @@ public class ListaUtils<T> {
         return intlistRes;
     }
 
-    static public List<Integer> interseccion(final List<Integer> listA, final List<Integer> listB) {
+    public static List<Integer> interseccion(final List<Integer> listA, final List<Integer> listB) {
         List<Integer> resultadoList = new ArrayList<Integer>();
         for (Integer date : listB) {
             if (listA.contains(date)) {
